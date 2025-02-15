2fe3251f5552946764846c5143253fd347f1fb54
Server <161137675+Gimkit0@users.noreply.github.com>
2025-02-15 17:35:14 -0600

    Update modules.lua

--- main/deps/modules.lua
+++ main/deps/modules.lua
@@ -2599,7 +2599,7 @@ function modules.UniversalCommands()
 							local root = char[aimPart]
 							local screenPos, visible = self.Camera:WorldToViewportPoint(root.Position)
 
-							if visible then
+							if visible and isTargetVisible(char) then
 								local magnitude = (Vector2.new(self.Mouse.X, self.Mouse.Y) - Vector2.new(screenPos.X, screenPos.Y)).Magnitude
 								if magnitude < dist and magnitude < circle.Radius then
 									dist = magnitude
