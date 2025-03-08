25e699d5060df7f2702505b33527ecb1d40b7b3e
Server <161137675+Gimkit0@users.noreply.github.com>
2025-03-07 19:55:52 -0600

    Update Remnants.lua

--- main/Remnants.lua
+++ main/Remnants.lua
@@ -46,7 +46,7 @@ function CommandBar.new(config)
 			SAVE_FILE_NAME = `{globalName}.rem`,
 			RELOAD_LOADSTRING  = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/Gimkit0/Gim-Test/refs/heads/main/main/Remnants.lua"))().new()]],
 			
-			VERSION = 1.02,
+			VERSION = 1.03,
 			VERSION_CHECKER_LINK = "https://raw.githubusercontent.com/Gimkit0/Gim-Test/refs/heads/main/CurrentVersion.lua",
 			
 			KEEP_ON_TELEPORT = true,
